ea8e73ec82351385c6e4e07ca7f77f0bae72250f
Yan Morozik <yan.morozik.2002@mail.ru>
2021-11-11 10:30:49 +0300

    change string json in application.java

--- src/main/java/eu/senla/library/Application.java
+++ src/main/java/eu/senla/library/Application.java
@@ -25,7 +25,7 @@ public class Application {
 
         String jsonStringBook1 = "{\"name\":\"some name1\",\"description\":\"description\",\"numberOfPage\":330,\"yearOfPublishing\":2002,\"numberOfCopies\":20,\"authors\":[{\"id\":1,\"firstName\":\"name\",\"surname\":\"surname\"},{\"id\":null,\"firstName\":\"name2\",\"surname\":\"surname2\"}],\"genre\":{\"id\":0,\"nameGenre\":\"detective\"},\"publishers\":[{\"id\":1,\"namePublisher\":\"some name\",\"telephone\":\"+375558336195\"},{\"id\":2,\"namePublisher\":\"some name2\",\"telephone\":\"+375558476196\"}],\"language\":{\"id\":0,\"nameLanguage\":\"English\"}}";
         String jsonStringBook2 = "{\"name\":\"some name2\",\"description\":\"description\",\"numberOfPage\":330,\"yearOfPublishing\":2002,\"numberOfCopies\":20,\"authors\":[{\"id\":1,\"firstName\":\"name\",\"surname\":\"surname\"},{\"id\":null,\"firstName\":\"name2\",\"surname\":\"surname2\"}],\"genre\":{\"id\":0,\"nameGenre\":\"detective\"},\"publishers\":[{\"id\":1,\"namePublisher\":\"some name\",\"telephone\":\"+375558336195\"},{\"id\":2,\"namePublisher\":\"some name2\",\"telephone\":\"+375558476196\"}],\"language\":{\"id\":0,\"nameLanguage\":\"English\"}}";
-        String jsonStringBook3 = "{\"name\":\"some name3\",\"description\":\"description\",\"numberOfPage\":330,\"yearOfPublishing\":2002,\"numberOfCopies\":20,\"authors\":[{\"id\":1,\"firstName\":\"name\",\"surname\":\"surname\"},{\"id\":null,\"firstName\":\"name2\",\"surname\":\"surname2\"}],\"genre\":{\"id\":0,\"nameGenre\":\"detective\"},\"publishers\":[{\"id\":1,\"namePublisher\":\"some name\",\"telephone\":\"+375558336195\"},{\"id\":2,\"namePublisher\":\"some name2\",\"telephone\":\"+375558476196\"}],\"language\":{\"id\":0,\"nameLanguage\":\"English\"}}";
+        String jsonStringBook3 = "{\"id\":0,\"name\":\"some name3\",\"description\":\"description\",\"numberOfPage\":330,\"yearOfPublishing\":2002,\"numberOfCopies\":20,\"authors\":[{\"id\":1,\"firstName\":\"name\",\"surname\":\"surname\"},{\"id\":null,\"firstName\":\"name2\",\"surname\":\"surname2\"}],\"genre\":{\"id\":0,\"nameGenre\":\"detective\"},\"publishers\":[{\"id\":1,\"namePublisher\":\"some name\",\"telephone\":\"+375558336195\"},{\"id\":2,\"namePublisher\":\"some name2\",\"telephone\":\"+375558476196\"}],\"language\":{\"id\":0,\"nameLanguage\":\"English\"}}";
 
         bookController.create(jsonStringBook1);
         bookController.create(jsonStringBook2); //вставляем 2 записи
@@ -44,7 +44,7 @@ public class Application {
 
         String jsonStringAuthor1 = "{\"firstName\":\"Alex\",\"surname\":\"Goncharov\"}";
         String jsonStringAuthor2 = "{\"firstName\":\"Fedya\",\"surname\":\"Pupkin\"}";
-        String jsonStringAuthor3 = "{\"firstName\":\"Vanya\",\"surname\":\"Markovkin\"}";
+        String jsonStringAuthor3 = "{\"id\":0,\"firstName\":\"Vanya\",\"surname\":\"Markovkin\"}";
 
         authorController.create(jsonStringAuthor1);
         authorController.create(jsonStringAuthor2);
@@ -61,8 +61,8 @@ public class Application {
 
         //////////////////////////////////////////// booking
 
-        String jsonStringBooking1 = "{\"id\":0,\"startTime\":1636494500475,\"endTime\":1636494500475,\"book\":{\"name\":\"luntik\"},\"user\":{\"firstName\":\"noname\",\"surname\":\"noname\"}}";
-        String jsonStringBooking2 = "{\"id\":1,\"startTime\":1636494500475,\"endTime\":1636494500475,\"book\":{\"name\":\"luntik\"},\"user\":{\"firstName\":\"noname\",\"surname\":\"noname\"}}";
+        String jsonStringBooking1 = "{\"startTime\":1636494500475,\"endTime\":1636494500475,\"book\":{\"name\":\"luntik\"},\"user\":{\"firstName\":\"noname\",\"surname\":\"noname\"}}";
+        String jsonStringBooking2 = "{\"startTime\":1636494500475,\"endTime\":1636494500475,\"book\":{\"name\":\"luntik\"},\"user\":{\"firstName\":\"noname\",\"surname\":\"noname\"}}";
         String jsonStringBooking3 = "{\"id\":0,\"startTime\":1636494500475,\"endTime\":1636494500475,\"book\":{\"name\":\"mur\"},\"user\":{\"firstName\":\"noname\",\"surname\":\"noname\"}}";
 
         bookingController.create(jsonStringBooking1);
@@ -80,8 +80,8 @@ public class Application {
 
         //////////////////////////////////////////// Genre
 
-        String jsonStringGenre1 = "{\"id\":0,\"nameGenre\":\"Detective\"}";
-        String jsonStringGenre2 = "{\"id\":1,\"nameGenre\":\"Detective\"}";
+        String jsonStringGenre1 = "{\"nameGenre\":\"Detective\"}";
+        String jsonStringGenre2 = "{\"nameGenre\":\"Detective\"}";
         String jsonStringGenre3 = "{\"id\":0,\"nameGenre\":\"Fantastic\"}";
 
         genreController.create(jsonStringGenre1);
@@ -99,8 +99,8 @@ public class Application {
 
         //////////////////////////////////////////// Language
 
-        String jsonStringLanguage1 = "{\"id\":0,\"nameLanguage\":\"English\"}";
-        String jsonStringLanguage2 = "{\"id\":1,\"nameLanguage\":\"English\"}";
+        String jsonStringLanguage1 = "{\"nameLanguage\":\"English\"}";
+        String jsonStringLanguage2 = "{\"nameLanguage\":\"English\"}";
         String jsonStringLanguage3 = "{\"id\":0,\"nameLanguage\":\"Russian\"}";
 
         languageController.create(jsonStringLanguage1);
@@ -118,8 +118,8 @@ public class Application {
 
         //////////////////////////////////////////// Publisher
 
-        String jsonStringPublisher1 = "{\"id\":0,\"namePublisher\":\"some name\",\"telephone\":\"+375443996486\"}";
-        String jsonStringPublisher2 = "{\"id\":1,\"namePublisher\":\"some name\",\"telephone\":\"+375443996486\"}";
+        String jsonStringPublisher1 = "{\"namePublisher\":\"some name\",\"telephone\":\"+375443996486\"}";
+        String jsonStringPublisher2 = "{\"namePublisher\":\"some name\",\"telephone\":\"+375443996486\"}";
         String jsonStringPublisher3 = "{\"id\":0,\"namePublisher\":\"just name\",\"telephone\":\"+375443996486\"}";
 
         publisherController.create(jsonStringPublisher1);
@@ -137,8 +137,8 @@ public class Application {
 
         //////////////////////////////////////////// Role
 
-        String jsonStringRole1 = "{\"id\":0,\"nameRole\":\"User\"}";
-        String jsonStringRole2 = "{\"id\":1,\"nameRole\":\"User\"}";
+        String jsonStringRole1 = "{\"nameRole\":\"User\"}";
+        String jsonStringRole2 = "{\"nameRole\":\"User\"}";
         String jsonStringRole3 = "{\"id\":0,\"nameRole\":\"Admin\"}";
 
         roleController.create(jsonStringRole1);
@@ -156,8 +156,8 @@ public class Application {
 
         //////////////////////////////////////////// User
 
-        String jsomStringUser1 = "{\"id\":0,\"firstName\":\"firstname\",\"surname\":\"surname\",\"telephone\":\"+375338569371\",\"login\":\"somelogin\",\"roles\":[{\"id\":1,\"nameRole\":\"User\"},{\"id\":null,\"nameRole\":null}]}";
-        String jsomStringUser2 = "{\"id\":1,\"firstName\":\"firstname\",\"surname\":\"surname\",\"telephone\":\"+375338569371\",\"login\":\"somelogin\",\"roles\":[{\"id\":1,\"nameRole\":\"User\"},{\"id\":null,\"nameRole\":null}]}";
+        String jsomStringUser1 = "{\"firstName\":\"firstname\",\"surname\":\"surname\",\"telephone\":\"+375338569371\",\"login\":\"somelogin\",\"roles\":[{\"id\":1,\"nameRole\":\"User\"},{\"id\":null,\"nameRole\":null}]}";
+        String jsomStringUser2 = "{\"firstName\":\"firstname\",\"surname\":\"surname\",\"telephone\":\"+375338569371\",\"login\":\"somelogin\",\"roles\":[{\"id\":1,\"nameRole\":\"User\"},{\"id\":null,\"nameRole\":null}]}";
         String jsomStringUser3 = "{\"id\":0,\"firstName\":\"another firstname\",\"surname\":\"surname\",\"telephone\":\"+375338569371\",\"login\":\"somelogin\",\"roles\":[{\"id\":1,\"nameRole\":\"User\"},{\"id\":null,\"nameRole\":null}]}";
 
         userController.create(jsomStringUser1);
